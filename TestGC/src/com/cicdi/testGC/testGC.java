e661eaa556b0ec974965f859fcd8b3f9aae1d976
田闯 <tianchuang@jsptpd.com>
2019-02-11 11:19:00 +0800

    发布

---
+++ TestGC/src/com/cicdi/testGC/testGC.java
@@ -0,0 +1,14 @@
+package com.cicdi.testGC;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+public class testGC {
+	public static void main(String[] args) {
+		List<GarbageCollectorMXBean> l = ManagementFactory.getGarbageCollectorMXBeans();
+		for(GarbageCollectorMXBean b:l) {
+			System.out.println(b.getName());
+		}
+	} 
+}
